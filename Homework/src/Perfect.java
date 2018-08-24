1ae7f2f6053b77680e6ca6e50b5dd473380c4187
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-08-24 19:02:06 +0300

    Homework version 2

--- Homework/src/Perfect.java
+++ Homework/src/Perfect.java
@@ -1,26 +1,26 @@
 public class Perfect {
     public static void PerfectNumber() {
         int i;
-        for(i = 1; i < 1000; i++) {
-            if(Number(i)) {
+        for (i = 1; i < 1000; i++) {
+            if (IsItThePerfectNumber(i)) {
                 System.out.println(i);
             }
         }
     }
-        public static boolean Number(int i) {
-            boolean Number = false;
-            int sum = 0;
-            int x;
-            for(x = 1; x < i; x++){
-                if(i % x == 0){
-                    sum = sum + x;
-                }
-            }
-            if(sum == i) {
-                Number = true;
-            }
-            return Number;
-        }
 
+    public static boolean IsItThePerfectNumber(int i) {
+        boolean Number = false;
+        int sum = 0;
+        int x;
+        for (x = 1; x < i; x++) {
+            if (i % x == 0) {
+                sum = sum + x;
+            }
+        }
+        if (sum == i) {
+            Number = true;
+        }
+        return Number;
+    }
 }
 
--- Homework/src/TestScoreStatistics.java
+++ Homework/src/TestScoreStatistics.java
@@ -4,10 +4,10 @@ public class TestScoreStatistics {
     public static void StudentScores() {
         Scanner s = new Scanner(System.in);
         int input = 0;
+        int min = 0;
         int counter = 0;
         int max = 1;
-        int min = 99;
-        int average = 1;
+        double average = 1;
         outer:
         while (input != 999) {
             System.out.println("Input the scores of the students: ");
@@ -21,7 +21,7 @@ public class TestScoreStatistics {
             if (input > max){max = input;}
             if (input < min){min = input;}
         }
-        System.out.println("Maximum score is: "+max+"\n  Lowest score is: "+min+"\n  Average score is:"+average/counter+"\n  Number of scores inputed = "+counter);
+        System.out.println(" Maximum score is: "+max+"\n Lowest score is: "+min+"\n Average score is:"+average/counter+"\n Number of scores inputed = "+counter);
     }
 }
 
