12d483d2cb38ab9014bd07a5a62293025f90b499
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-09-01 23:18:43 +0300

    Homework version 2

--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -1,7 +1,7 @@
-import VacationHomeWrk.FlowControl;
-
 public class Main {
     public static void main(String[] args) {
-        FlowControl.exFour();
+        //DataTypes #6: 4 or 5 does not work.(how to determine if an element of the array is null?)
+        //FlowControl #4: if user selects 1 1 1 and the computer selects 1 2 3, it finds 3 matches.
+
     }
 }
--- Homework/src/VacationHomeWrk/FlowControl.java
+++ Homework/src/VacationHomeWrk/FlowControl.java
@@ -78,7 +78,7 @@ public class FlowControl {
         }
     }
 
-    public static void exFour(){
+    public static void exFour() {
         Random rand = new Random();
         int a = 1;//rand.nextInt(9);
         int b = 2;//rand.nextInt(9);
@@ -92,40 +92,137 @@ public class FlowControl {
         input1 = s.nextInt();
         input2 = s.nextInt();
         input3 = s.nextInt();
-        if (input1 == a || input1 == b || input1 == c){
+        if (input1 == a || input1 == b || input1 == c) {
             award += 1;
         }
-        if (input2 == a || input2 == b || input2 == c){
+        if (input2 == a || input2 == b || input2 == c) {
             award += 1;
         }
-        if (input3 == a || input3 == b || input3 == c){
+        if (input3 == a || input3 == b || input3 == c) {
             award += 1;
         }
-        if(input1 == a && input2 == b && input3 == c){
+        if (input1 == a && input2 == b && input3 == c) {
             award = 4;
         }
-        switch (award){
+        switch (award) {
             case 0:
-                System.out.println("You choose the following numbers: " +input1+"  "+input2+"  "+input3);
-                System.out.println("The lottery numbers are: "+a+"  "+b+"  "+c);
-                System.out.println("You did not guess anything... "); break;
+                System.out.println("You choose the following numbers: " + input1 + "  " + input2 + "  " + input3);
+                System.out.println("The lottery numbers are: " + a + "  " + b + "  " + c);
+                System.out.println("You did not guess anything... ");
+                break;
             case 1:
-                System.out.println("You choose the following numbers: " +input1+"  "+input2+"  "+input3);
-                System.out.println("The lottery numbers are: "+a+"  "+b+"  "+c);
-                System.out.println("You won: $10"); break;
+                System.out.println("You choose the following numbers: " + input1 + "  " + input2 + "  " + input3);
+                System.out.println("The lottery numbers are: " + a + "  " + b + "  " + c);
+                System.out.println("You won: $10");
+                break;
             case 2:
-                System.out.println("You choose the following numbers: " +input1+"  "+input2+"  "+input3);
-                System.out.println("The lottery numbers are: "+a+"  "+b+"  "+c);
-                System.out.println("You won: $100"); break;
+                System.out.println("You choose the following numbers: " + input1 + "  " + input2 + "  " + input3);
+                System.out.println("The lottery numbers are: " + a + "  " + b + "  " + c);
+                System.out.println("You won: $100");
+                break;
             case 3:
-                System.out.println("You choose the following numbers: " +input1+"  "+input2+"  "+input3);
-                System.out.println("The lottery numbers are: "+a+"  "+b+"  "+c);
-                System.out.println("You won: $1,000"); break;
+                System.out.println("You choose the following numbers: " + input1 + "  " + input2 + "  " + input3);
+                System.out.println("The lottery numbers are: " + a + "  " + b + "  " + c);
+                System.out.println("You won: $1,000");
+                break;
             case 4:
-                System.out.println("You choose the following numbers: " +input1+"  "+input2+"  "+input3);
-                System.out.println("The lottery numbers are: "+a+"  "+b+"  "+c);
-                System.out.println("You won: $1,000,000"); break;
+                System.out.println("You choose the following numbers: " + input1 + "  " + input2 + "  " + input3);
+                System.out.println("The lottery numbers are: " + a + "  " + b + "  " + c);
+                System.out.println("You won: $1,000,000");
+                break;
         }
+    }
+
+    public static void exFive() {
+        int input;
+        Random choice = new Random();
+        Scanner s = new Scanner(System.in);
+        int aiInput = choice.nextInt(3) + 1;
+        System.out.println("Select: 1 - Rock; 2 - Scissors; 3 - Paper");
+        input = s.nextInt();
+        if (input == aiInput) {
+            System.out.println("It's a tie!");
+        } else if (input == 2 && aiInput == 3) {
+            System.out.println("You won! I've choose Paper.");
+        } else if (input == 1 && aiInput == 2) {
+            System.out.println("You won! I've choose Scissors.");
+        } else if (input == 3 && aiInput == 1) {
+            System.out.println("You won! I've choose Rock.");
+        } else {
+            System.out.println("You lost! I've choose: " + aiInput);
+        }
+    }
+
+    public static void exSix() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (i == j) {
+                    System.out.print("O");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void exSeven() {
+        double mex = 121;
+        double us = 315;
+        int i;
+        for (i = 1; mex < us; i++) {
+            System.out.println(i + " year(s):");
+            mex *= 1.0101;
+            System.out.printf("Population of Mexico: %.2f mln.\n", mex);
+            us *= 0.9985;
+            System.out.printf("Population of US: %.2f mln.\n", us);
+        }
+    }
+
+    public static void exEight() {
+        int art;
+        int bob;
+        int cal;
+        int dan;
+        int eli;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input Art's scores: ");
+        art = s.nextInt();
+        System.out.println("Input Bob's scores: ");
+        bob = s.nextInt();
+        System.out.println("Input Cal's scores: ");
+        cal = s.nextInt();
+        System.out.println("Input Dan's scores: ");
+        dan = s.nextInt();
+        System.out.println("Input Eli's scores: ");
+        eli = s.nextInt();
+
+        System.out.println("    Points for game:\n");
+        System.out.print("Art   ");
+        for (int i = 0; i < art; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("Bob   ");
+        for (int i = 0; i < bob; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("Cal   ");
+        for (int i = 0; i < cal; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("Dan   ");
+        for (int i = 0; i < dan; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("Eli   ");
+        for (int i = 0; i < eli; i++) {
+            System.out.print("*");
+        }
+
 
     }
 
@@ -143,10 +240,7 @@ public class FlowControl {
             }
             sum += player;
             if (sum == 21) {
-                System.out.println("You won, the result is: " + sum);
-            }
-            if (sum > 21) {
-                System.out.println("You lost, the result is: " + sum);
+                System.out.println("You won, the result is: 21");
             }
             if (sum <= 14) {
                 sum += n;
