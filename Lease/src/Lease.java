d90f7ea18d6021b6351d296308d88f9545da4cf9
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-09-04 22:58:33 +0300

    Lease app

--- Lease/src/Lease.java
+++ Lease/src/Lease.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lease {
     private String name;
     private int number;
@@ -11,7 +13,35 @@ public class Lease {
         this.term = 12;
     }
 
+    public void showValues(){
+        System.out.println("Name of the apartment: " + getName());
+        System.out.println("Number of the apartment: " + getNumber());
+        System.out.println("Rent of the apartment: " + getRent());
+        System.out.println("Terms of the rent: " + getTerm() + "\n");
 
+    }
+
+    public void addPetFee(){
+        rent += 10;
+        explainPetPolicy();
+    }
+    private static void explainPetPolicy(){
+        System.out.println("If you have a pet, the rent will increase by 10$ per month!");
+    }
+
+    public void getData(){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Input the name: ");
+        setName(s.nextLine());
+        System.out.println("Input the number: ");
+        setNumber(s.nextInt());
+        System.out.println("Input the rent: ");
+        setRent(s.nextInt());
+        System.out.println("Input the terms: ");
+        setTerm(s.nextInt());
+
+    }
 
     public String getName() {
         return name;
@@ -44,9 +74,4 @@ public class Lease {
     public void setTerm(int term) {
         this.term = term;
     }
-
-    public static void main(String[] args) {
-
-    }
-
 }
---
+++ Lease/src/TestLease.java
@@ -0,0 +1,20 @@
+public class TestLease {
+    public static void main(String[] args) {
+        Lease object1 = new Lease();
+        Lease object2 = new Lease();
+        Lease object3 = new Lease();
+        Lease object4 = new Lease();
+
+        object1.getData();
+        object2.getData();
+        object3.getData();
+
+        object1.showValues();
+        object1.addPetFee();
+        object1.showValues();
+
+        object2.showValues();
+        object3.showValues();
+        object4.showValues();
+    }
+}
