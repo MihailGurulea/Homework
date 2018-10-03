3bccd732f6460fa3bd7d3e6a57f54577ec778802
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-10-03 19:32:43 +0300

    Construct ID

--- StringHomework/src/IonExercises/PhoneNumberFormat.java
+++ StringHomework/src/IonExercises/PhoneNumberFormat.java
@@ -6,7 +6,6 @@ public class PhoneNumberFormat {
     public static void PhoneFormat(){
         int sentinel = 0;
         Scanner s = new Scanner(System.in);
-        label:
         while(sentinel != 999){
             System.out.println("Please input a 10 digit phone number: ");
             String newString = s.nextLine();
@@ -17,12 +16,12 @@ public class PhoneNumberFormat {
 
             if(newString.length() > 10){
                 System.out.println("Your number contains more than 10 digits!");
-                continue label;
+                continue;
             }
 
             if(newString.length() < 10){
                 System.out.println("Your number contains less than 10 digits!");
-                continue label;
+                continue;
             }
 
             System.out.println("Formated number:" + "(" + newString.substring(0,3) + ")" + " " + newString.substring(3,7) + "-" + newString.substring(7));
