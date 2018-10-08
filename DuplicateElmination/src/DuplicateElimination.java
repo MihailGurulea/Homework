f5b74b7f25c8655d1d116959127135ec287e1c14
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-10-08 16:44:20 +0300

    Duplicate Elimination

--- DuplicateElmination/src/DuplicateElimination.java
+++ DuplicateElmination/src/DuplicateElimination.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DuplicateElimination {
-    private static boolean isNotPresent(int input, int[] array){
+    private static boolean isNotPresent(int input, int[] array) {
         for (int anArray : array) {
             if (anArray == input) {
                 return false;
@@ -14,25 +14,30 @@ public class DuplicateElimination {
         int[] intArray = new int[5];
         Scanner s = new Scanner(System.in);
         int counter = 0;
-        int temp = 0;
+        int temp;
 
-        while(intArray[4] == 0){
+        while (intArray[4] == 0) {
             System.out.println("Please input a number between 10 and 100: ");
             temp = s.nextInt();
-            if(temp > 10 && temp <= 100) {
+            if (temp > 10 && temp <= 100) {
                 if (DuplicateElimination.isNotPresent(temp, intArray)) {
                     intArray[counter] = temp;
-                    continue;
+                    counter++;
                 }
-                if (counter > 0) {
+                if (counter < intArray.length) {
                     for (int anIntArray : intArray) {
                         if (anIntArray != 0) {
                             System.out.println(anIntArray);
                         }
                     }
                 }
-                counter++;
+            } else {
+                System.out.println("Your number is out of range 10-100!");
+                continue;
             }
+        }
+        for(int i : intArray){
+            System.out.println(i);
         }
     }
 }
