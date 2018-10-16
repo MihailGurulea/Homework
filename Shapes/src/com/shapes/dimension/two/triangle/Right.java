29d276ee29481b1c94d346f20d8640c5d8e986f2
Mihail Gurulea <mihail.gurulea@gmail.com>
2018-10-16 21:33:37 +0300

    Shapes

--- Shapes/src/com/shapes/dimension/two/triangle/Right.java
+++ Shapes/src/com/shapes/dimension/two/triangle/Right.java
@@ -2,6 +2,6 @@ package com.shapes.dimension.two.triangle;
 
 import com.shapes.dimension.two.Triangles;
 
-public abstract class Right extends Triangles {
+public class Right extends Triangles {
 
 }
